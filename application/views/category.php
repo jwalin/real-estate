e61ffaff1c4ca13b650904601c9ff710fa133017
Bhavik <bhavikatb@gmail.com>
2020-01-31 11:43:56 +0530

    latest code

---
+++ application/views/category.php
@@ -0,0 +1,59 @@
+<div class="page-wrapper">
+    <div class="content container-fluid">
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card mb-0">
+                    <div class="card-header">
+                        <h4 class="card-title mb-0">Category</h4>
+                        <div class="col-auto float-right ml-auto">
+                            <a href="#" class="btn add-btn" data-toggle="modal" data-target="#add_employee"><i class="fa fa-plus"></i> Add Employee</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+
+                        <div class="table-responsive">
+                            <table class="datatable table table-stripped mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Tiger Nixon</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Garrett Winters</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Ashton Cox</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="add_employee" class="modal custom-modal fade" role="dialog">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Add Employee</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+
+    </div>
