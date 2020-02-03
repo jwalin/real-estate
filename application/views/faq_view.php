f39b7c434606639ad30d7db463ed48615ec95b97
Bhavik <bhavikatb@gmail.com>
2020-02-03 12:52:57 +0530

    Latest code

---
+++ application/views/faq_view.php
@@ -0,0 +1,81 @@
+<div class="content container-fluid">
+
+    <!-- Page Header -->
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3 class="page-title">FAQ</h3>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?php echo base_url('home'); ?>">Home</a></li>
+                    <li class="breadcrumb-item active">FAQ</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /Page Header -->
+
+
+    <div class="faq-card">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a class="collapsed" data-toggle="collapse" href="#collapseOne">Faq Title</a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="card-collapse collapse" style="margin-top: 20px;">
+                <div class="card-body">
+                    <p>Faq Title Faq Title Faq Title Faq Title Faq Title.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a class="collapsed" data-toggle="collapse" href="#collapseTwo">Faq Title</a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="card-collapse collapse" style="margin-top: 20px;">
+                <div class="card-body">
+                    <p>Faq Title Faq Title Faq Title Faq Title Faq Title.</p>
+                    </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a class="collapsed" data-toggle="collapse" href="#collapseThree">Faq Title</a>
+                </h4>
+            </div>
+            <div id="collapseThree" class="card-collapse collapse" style="margin-top: 20px;">
+                <div class="card-body">
+                    <p>Faq Title Faq Title Faq Title Faq Title Faq Title.</p>
+                    </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a class="collapsed" data-toggle="collapse" href="#collapseFour">Faq Title</a>
+                </h4>
+            </div>
+            <div id="collapseFour" class="card-collapse collapse" style="margin-top: 20px;">
+                <div class="card-body">
+                    <p>Faq Title Faq Title Faq Title Faq Title Faq Title..</p>
+                    </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a class="collapsed" data-toggle="collapse" href="#collapseFive">Faq Title</a>
+                </h4>
+            </div>
+            <div id="collapseFive" class="card-collapse collapse" style="margin-top: 20px;">
+                <div class="card-body">
+                    <p>Faq Title Faq Title Faq Title Faq Title Faq Title.</p>
+                     </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Page Content -->
--- application/views/include/sidebar_new.php
+++ application/views/include/sidebar_new.php
@@ -1,38 +1,49 @@
 <div class="sidebar" id="sidebar">
-                <div class="sidebar-inner slimscroll">
-					<div id="sidebar-menu" class="sidebar-menu">
-						<ul>
-							
-                                                    <li class="<?php if ($this->router->fetch_class() == 'home') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('home'); ?>"><i class="la la-home"></i> <span>Home</span></a>
-							</li>
-                                                        
-                                                    <li class="<?php if ($this->router->fetch_class() == 'profile') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('profile'); ?>"><i class="la la-user"></i> <span>Profile</span></a>
-							</li>
-							
-							<li class="<?php if ($this->router->fetch_class() == 'category') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('category'); ?>"><i class="la la-user-secret"></i> <span>Trade Category</span></a>
-							</li>
-							<li class="<?php if ($this->router->fetch_class() == 'defect_types') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('defect_types'); ?>"><i class="la la-ticket"></i> <span>Defect Types</span></a>
-							</li>
-							<li class="<?php if ($this->router->fetch_class() == 'partner') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('partner'); ?>"><i class="fa fa-handshake-o"></i> <span>Trade Partners</span></a>
-							</li>
-							<li class="<?php if ($this->router->fetch_class() == 'builder') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('builder'); ?>"><i class="la la-users"></i> <span>Builder</span></a>
-							</li>
-							<li class="<?php if ($this->router->fetch_class() == 'defect_location') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('defect_location'); ?>"><i class="la la-map-marker"></i> <span>Defect Location</span></a>
-							</li>
-                                                        <li class="<?php if ($this->router->fetch_class() == 'lots_tracks') { ?> active <?php } ?>"> 
-								<a href="<?php echo base_url('lots_tracks'); ?>"><i class="la la-external-link"></i> <span>Lots & Tracks</span></a>
-							</li>
-							
-							
-						</ul>
-					</div>
-                </div>
-            </div>
-			<!-- /Sidebar -->
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+
+                <li class="<?php if ($this->router->fetch_class() == 'home') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('home'); ?>"><i class="la la-home"></i> <span>Home</span></a>
+                </li>
+
+                <li class="<?php if ($this->router->fetch_class() == 'profile') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('profile'); ?>"><i class="la la-user"></i> <span>Profile</span></a>
+                </li>
+
+                <li class="<?php if ($this->router->fetch_class() == 'category') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('category'); ?>"><i class="la la-user-secret"></i> <span>Trade Category</span></a>
+                </li>
+                <li class="<?php if ($this->router->fetch_class() == 'defect_types') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('defect_types'); ?>"><i class="la la-ticket"></i> <span>Defect Types</span></a>
+                </li>
+                <li class="<?php if ($this->router->fetch_class() == 'partner') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('partner'); ?>"><i class="fa fa-handshake-o"></i> <span>Trade Partners</span></a>
+                </li>
+                <li class="<?php if ($this->router->fetch_class() == 'builder') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('builder'); ?>"><i class="la la-users"></i> <span>Builder</span></a>
+                </li>
+                <li class="<?php if ($this->router->fetch_class() == 'defect_location') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('defect_location'); ?>"><i class="la la-map-marker"></i> <span>Defect Location</span></a>
+                </li>
+                <li class="<?php if ($this->router->fetch_class() == 'lots_tracks') { ?> active <?php } ?>"> 
+                    <a href="<?php echo base_url('lots_tracks'); ?>"><i class="la la-external-link"></i> <span>Lots & Tracks</span></a>
+                </li>
+                <li class="submenu">
+                    <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="la la-bars"></i><span class="nav-text">Support</span>
+                    </a>
+                    <ul aria-expanded="false">
+                        <li class="<?php if ($this->router->fetch_class() == 'support/faq') { ?> active <?php } ?>"><a href="<?php echo base_url('support/faq'); ?>">Faq</a></li>
+                        <li class="<?php if ($this->router->fetch_class() == 'support/contact_support') { ?> active <?php } ?>"><a href="<?php echo base_url('support/contact_support'); ?>">Contact Support</a></li>
+                        <li class="<?php if ($this->router->fetch_class() == 'support/tutorial') { ?> active <?php } ?>"><a href="<?php echo base_url('support/tutorial'); ?>">Tutorial</a></li> 
+
+                    </ul>
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /Sidebar -->
